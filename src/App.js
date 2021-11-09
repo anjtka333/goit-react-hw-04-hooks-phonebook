a2752102d9ab13d1e7b1b638013ecc7178472077
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-11-09 18:08:01 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,88 +1,98 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4, v4 } from "uuid";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import Filter from "./components/Filter/Filter";
 import s from "./components/ContactForm/ContactForm.module.scss";
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: "",
-  };
+const App = () => {
+  // state = {
+  //   contacts: [],
+  //   filter: "",
+  // };
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
 
-  addContact = (text) => {
+  const addContact = (text) => {
     const { name, number } = text;
-
     if (
-      this.state.contacts.some((item) => {
+      contacts.some((item) => {
         return item.name.toLowerCase().includes(name.toLowerCase());
       })
     ) {
       alert(`${name} is already in contacts`);
       return;
     }
-
     const contact = {
       id: v4(),
       name: name,
       number: number,
     };
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
+    // this.setState(({ contacts }) => ({
+    //   contacts: [contact, ...contacts],
+    // }));
+    setContacts((prevContacts) => [contact, ...prevContacts]);
   };
 
-  handleFilterChange = (e) => {
-    this.setState({
-      filter: e.currentTarget.value,
-    });
+  const handleFilterChange = (e) => {
+    // this.setState({
+    //   filter: e.currentTarget.value,
+    // });
+
+    setFilter(e.currentTarget.value);
   };
 
-  filter = () => {
-    const filteredContacts = this.state.contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const filteredContacts = () => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
-    return filteredContacts;
   };
 
-  delateItem = (id) => {
-    return this.setState((prevState) => {
-      return {
-        contacts: prevState.contacts.filter((item) => item.id !== id),
-      };
-    });
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parsedContacts = JSON.parse(contacts)||[];
-    this.setState({ contacts: parsedContacts });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter
-          value={this.state.filter}
-          onChange={this.handleFilterChange}
-          inputStyle={s.input}
-        />
-        <ContactList contactsFiltered={this.filter} onClick={this.delateItem} />
-      </div>
+  const delateItem = (id) => {
+    // return this.setState((prevState) => {
+    //   return {
+    //     contacts: prevState.contacts.filter((item) => item.id !== id),
+    //   };
+    // });
+    return setContacts((prevContacts) =>
+      prevContacts.filter((item) => item.id !== id)
     );
-  }
-}
+  };
+
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem("contacts");
+  //   const parsedContacts = JSON.parse(contacts)||[];
+  //   this.setState({ contacts: parsedContacts });
+  // }
+
+  useEffect(() => {
+    const contacts = JSON.parse(localStorage.getItem("contacts"));
+    contacts && setContacts(contacts);
+  }, []);
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  //   }
+  // }
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="App">
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter
+        value={filter}
+        onChange={handleFilterChange}
+        inputStyle={s.input}
+      />
+      <ContactList contactsFiltered={filteredContacts} onClick={delateItem} />
+    </div>
+  );
+};
 
 export default App;
-
-
